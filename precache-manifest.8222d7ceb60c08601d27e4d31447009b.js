e2c27a15b73daab4006d335e3928def169d6c0df
Nicole Reed <nickreed033@gmail.com>
2020-10-26 16:17:26 +0300

    Updates

---
+++ precache-manifest.8222d7ceb60c08601d27e4d31447009b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d9b7b16121bb92d986032995d1bcfe",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "fb03dca30d11a759ff84",
+    "url": "/calculator/static/css/main.90662a75.chunk.css"
+  },
+  {
+    "revision": "99c1e1417c7598b40d93",
+    "url": "/calculator/static/js/2.48b1a484.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/calculator/static/js/2.48b1a484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb03dca30d11a759ff84",
+    "url": "/calculator/static/js/main.5c2b51a5.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
